b18d330bad0e8a8f927b07f9303a01e5b7e23684
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-02 15:17:16 -0700

    fixing date

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """class BaseModel."""
 
-import sys
-sys.path.append('/root/holbertonschool-AirBnB_clone/models/')
+
 import uuid
 from datetime import datetime
 
@@ -12,7 +11,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.create_at = datetime.now()
-        self.updated_at = datetine.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -21,7 +20,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        new_dict = dict(self.__dict)
+        new_dict = self.__dict__.copy()
+        new_dict = dict(self.__dict__)
         new_dict["class"] = self.__class__.__name__
         new_dict["create_at"]= self.create_at.isoformat()
         new_dict["updated_at"]= self.updated_at.isoformat()
