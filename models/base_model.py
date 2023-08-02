23ba072ad5691b79d122221a95cdecbfb2d741bc
yelsy <yelsycg17@gmail.com>
2023-08-02 15:54:59 -0700

    adding commit to the methods

--- models/base_model.py
+++ models/base_model.py
@@ -7,23 +7,26 @@ from datetime import datetime
 
 
 class BaseModel:
-    """metodo de instancia Basemodel"""
+    """Represents baseModel of the project"""
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.create_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Return the str representation of the BaseModel instance"""
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
 
     def save(self):
+        """Update  with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance"""
         new_dict = self.__dict__.copy()
         new_dict = dict(self.__dict__)
         new_dict["class"] = self.__class__.__name__
-        new_dict["create_at"]= self.create_at.isoformat()
-        new_dict["updated_at"]= self.updated_at.isoformat()
+        new_dict["create_at"] = self.create_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-
