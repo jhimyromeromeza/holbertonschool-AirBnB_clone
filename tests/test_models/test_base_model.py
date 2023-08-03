8b5cb5725475ba033ba8bc90cafdaee8233905e2
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-02 19:50:26 -0700

    test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
+
+from models.base_model import BaseModel
 import unittest
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel()
+
+    def test_init(self):
+        self.assertTrue(hasattr(self.model, "id"))
+        self.assertTrue(hasattr(self.model, "created_at"))
+        self.assertTrue(hasattr(self.model, "updated_at"))
+
+    def test_str(self):
+        expected_output = "[BaseModel] ({}) {}".format(self.model.id, self.model.__dict__)
+        self.assertEqual(str(self.model), expected_output)
+
+    def test_save(self):
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(old_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        model_dict = self.model.to_dict()
+
+    self.assertEqual(model_dict["__class__"], "BaseModel")
+
+    self.assertEqual(model_dict["created_at"], self.model.created_at.isoformat())
+
+    self.assertEqual(model_dict["updated_at"], self.model.updated_at.isoformat())
+
+    if __name__ == "__main__":
+        unittest.main()
