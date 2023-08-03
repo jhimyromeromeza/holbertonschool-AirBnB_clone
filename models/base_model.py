7d439a46c1baca796734001d996745b2c29f387f
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-02 21:27:48 -0700

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,18 @@ from datetime import datetime
 class BaseModel:
     """Represents baseModel of the project"""
     def __init__(self, *args, **kwargs):
+
         if kwargs not is None:
             for key, value in kwargs.items():
                 if key, value == create_at or update_at:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.create_at = datetime.now()
-                    self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.create_at = datetime.now()
+            self.updated_at = datetime.now()
+                    
 
     def __str__(self):
         """Return the str representation of the BaseModel instance"""
