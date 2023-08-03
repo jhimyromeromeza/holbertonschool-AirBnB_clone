85b074e4163ad6390c68930e2e83d8aa660c6c9c
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-03 15:03:16 -0700

    update base_model identation

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-       if len(kwargs) != 0:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == created_at or key == updated_at:
                     formobj = datetime.strptime(value, dtform)
