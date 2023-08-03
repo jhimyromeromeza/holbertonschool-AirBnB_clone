2c68ea395de6551a5237961ace5ec7f217419a1d
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-03 14:40:27 -0700

    base model uptodate

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,16 @@ from datetime import datetime
 class BaseModel:
     """Represents baseModel of the project"""
     def __init__(self, *args, **kwargs):
-
-        if kwargs not is None:
+        dtform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.create_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key, value == create_at or update_at:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    formobj = datetime.strptime(value, dtform)
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.create_at = datetime.now()
-            self.updated_at = datetime.now()
-                    
 
     def __str__(self):
         """Return the str representation of the BaseModel instance"""
