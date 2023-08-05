668387a67f0391022c7136293f7cf84119870210
yelsy <yelsycg17@gmail.com>
2023-08-04 18:07:45 -0700

    module for class state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Module for state class"""
+
+
+class State(BaseModel):
+    """
+    public attr:
+    name: empty string
+    """
+    name = ""
