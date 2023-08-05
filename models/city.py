c2417a9d75b3676fef53b7dcee89662a4c869d36
yelsy <yelsycg17@gmail.com>
2023-08-04 18:15:59 -0700

    module for the class city

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""module for the class City"""
+
+
+class City(BaseModel):
+    """
+    state_id (str): The state id
+    name: str name of the city
+    """
+    state_id = ""
+    name = ""
