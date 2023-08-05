a1c8aaab901dec8cf9d85fe6529a2dd94e8c25e8
yelsy <yelsycg17@gmail.com>
2023-08-04 18:33:43 -0700

    module for the class amenity

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Module for the class Amenity"""
+
+
+class Amenity(BaseModel):
+    """
+    Attr:
+    name: a string the name of the amenity
+    """
+    name = ""
